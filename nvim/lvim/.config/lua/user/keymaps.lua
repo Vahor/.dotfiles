550eddc2d1865b02a13be4fb075737483ac24ed8
Vahor <me@vahor.fr>
2024-03-01 23:33:18 +0100

    tmux

---
+++ nvim/lvim/.config/lua/user/keymaps.lua
@@ -0,0 +1,12 @@
+lvim.keys.normal_mode["p"] = {
+ "<cmd>bp<cr>",
+  "prev buffer"
+}
+
+lvim.builtin.which_key.mappings["n"] = {
+ "<cmd>bn<CR>", "Next buffer"
+}
+
+lvim.builtin.which_key.mappings["P"] = {
+  "<cmd>lua require'telescope'.extensions.project.project{}<CR>", "Projects"
+}
---
+++ nvim/lvim/.config/lua/user/plugins.lua
@@ -0,0 +1,57 @@
+lvim.plugins = {
+  "stevearc/dressing.nvim",        -- popup in nvim
+  "ChristianChiarulli/swenv.nvim", -- Switch python env
+  -- python
+  "mfussenegger/nvim-dap-python",
+  "nvim-neotest/neotest",
+  "nvim-neotest/neotest-python",
+
+  -- rust
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^4', -- Recommended
+    ft = { 'rust' },
+  },
+
+
+  {
+    "tpope/vim-fugitive",
+    cmd = {
+      "G",
+      "Git",
+      "Gdiffsplit",
+      "Gread",
+      "Gwrite",
+      "Ggrep",
+      "GMove",
+      "GDelete",
+      "GBrowse",
+      "GRemove",
+      "GRename",
+      "Glgrep",
+      "Gedit"
+    },
+    ft = { "fugitive" }
+  },
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    event = "BufRead",
+  },
+  {
+    "sindrets/diffview.nvim",
+    event = "BufRead",
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    after = { "copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+  "roobert/tailwindcss-colorizer-cmp.nvim",
+}

949b5c4a5f78bd071be9d6c356c2ba5ffd5971ad
Vahor <me@vahor.fr>
2024-05-23 09:31:13 +0200

    supermaven

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,29 +1,41 @@
 return {
+  -- {
+  --   'zbirenbaum/copilot.lua',
+  --   event = 'InsertEnter',
+  --   build = ':Copilot auth',
+  --   config = function()
+  --     require('copilot').setup {
+  --       filetypes = {
+  --         markdown = true,
+  --       },
+  --       suggestion = {
+  --         auto_trigger = true,
+  --         keymap = {
+  --           accept = false, -- Defined in the config below
+  --           next = '<c-n>',
+  --           prev = '<c-p>',
+  --         },
+  --       },
+  --     }
+  --     vim.keymap.set('i', '<Tab>', function()
+  --       if require('copilot.suggestion').is_visible() then
+  --         require('copilot.suggestion').accept()
+  --       else
+  --         vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, false, true), 'n', false)
+  --       end
+  --     end, { desc = 'Super Tab' })
+  --   end,
+  -- },
   {
-    'zbirenbaum/copilot.lua',
-    event = 'InsertEnter',
-    build = ':Copilot auth',
+    'supermaven-inc/supermaven-nvim',
     config = function()
-      require('copilot').setup {
-        filetypes = {
-          markdown = true,
-        },
-        suggestion = {
-          auto_trigger = true,
-          keymap = {
-            accept = false, -- Defined in the config below
-            next = '<c-n>',
-            prev = '<c-p>',
-          },
+      require('supermaven-nvim').setup {
+        keymaps = {
+          accept_suggestion = '<Tab>',
+          clear_suggestion = '<C-]>',
+          accept_suggestion_and_insert = '<C-j>',
         },
       }
-      vim.keymap.set('i', '<Tab>', function()
-        if require('copilot.suggestion').is_visible() then
-          require('copilot.suggestion').accept()
-        else
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, false, true), 'n', false)
-        end
-      end, { desc = 'Super Tab' })
     end,
   },
 }

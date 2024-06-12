12c0a84deb7f7bb73aa23cda4f1c919b07f1b7a1
Vahor <me@vahor.fr>
2024-06-12 13:10:29 +0200

    add lualine-ex for relative filename

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -2,17 +2,25 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     event = 'VeryLazy',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { 'nvim-tree/nvim-web-devicons', 'dokwork/lualine-ex' },
     opts = {
       sections = {
+        lualine_a = { {
+          'mode',
+          fmt = function(res)
+            return res:sub(1, 3)
+          end,
+        } },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = {
           { 'harpoon2', indicators = { '1', '2', '3', '4' }, active_indicators = { '[1]', '[2]', '[3]', '[4]' } },
           {
-            'filename',
-            file_status = true,
-            path = 1,
+            'ex.relative_filename',
+            max_length = 0.5,
           },
         },
+        lualine_x = { 'filetype' },
+        lualine_y = {},
       },
     },
   },

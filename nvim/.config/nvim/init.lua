abada363a92030bdc82b44e771f730aa7781fa4c
Vahor <me@vahor.fr>
2024-08-09 09:33:48 +0200

    silent supermaven

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -549,6 +549,7 @@ require('lazy').setup {
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+          { name = 'supermaven' },
         },
       }
     end,
--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,35 +1,9 @@
 return {
-  -- {
-  --   'zbirenbaum/copilot.lua',
-  --   event = 'InsertEnter',
-  --   build = ':Copilot auth',
-  --   config = function()
-  --     require('copilot').setup {
-  --       filetypes = {
-  --         markdown = true,
-  --       },
-  --       suggestion = {
-  --         auto_trigger = true,
-  --         keymap = {
-  --           accept = false, -- Defined in the config below
-  --           next = '<c-n>',
-  --           prev = '<c-p>',
-  --         },
-  --       },
-  --     }
-  --     vim.keymap.set('i', '<Tab>', function()
-  --       if require('copilot.suggestion').is_visible() then
-  --         require('copilot.suggestion').accept()
-  --       else
-  --         vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, false, true), 'n', false)
-  --       end
-  --     end, { desc = 'Super Tab' })
-  --   end,
-  -- },
   {
     'supermaven-inc/supermaven-nvim',
     config = function()
       require('supermaven-nvim').setup {
+        log_level = 'off',
         keymaps = {
           accept_suggestion = '<Tab>',
           clear_suggestion = '<C-]>',

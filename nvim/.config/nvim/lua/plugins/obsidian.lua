ec2b103f5746ca697dafdf757e42eaf7de48ab14
Vahor <me@vahor.fr>
2025-03-16 16:38:30 +0100

    add obsidian plugin

---
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,108 @@
+return {
+  'obsidian-nvim/obsidian.nvim',
+  version = '*', -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = 'markdown',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  opts = {
+    workspaces = {
+      {
+        name = 'personal',
+        path = '~/Library/Mobile Documents/iCloud~md~obsidian/Documents/notes',
+      },
+    },
+    completion = {
+      -- Enables completion using blink.cmp
+      blink = true,
+      min_chars = 1,
+    },
+    -- Optional, configure additional syntax highlighting / extmarks.
+    -- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
+    ui = {
+      enable = true, -- set to false to disable all additional syntax features
+      update_debounce = 200, -- update delay after a text change (in milliseconds)
+      max_file_length = 5000, -- disable UI features for files with more than this many lines
+      -- Define how various check-boxes are displayed
+      checkboxes = {
+        -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+        [' '] = { char = '󰄱', hl_group = 'ObsidianTodo' },
+        ['x'] = { char = '', hl_group = 'ObsidianDone' },
+        -- ['>'] = { char = '', hl_group = 'ObsidianRightArrow' },
+        -- ['~'] = { char = '󰰱', hl_group = 'ObsidianTilde' },
+        -- ['!'] = { char = '', hl_group = 'ObsidianImportant' },
+        -- Replace the above with this if you don't have a patched font:
+        -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+        -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+
+        -- You can also add more custom ones...
+      },
+      -- Use bullet marks for non-checkbox lists.
+      bullets = { char = '•', hl_group = 'ObsidianBullet' },
+      external_link_icon = { char = '', hl_group = 'ObsidianExtLinkIcon' },
+      -- Replace the above with this if you don't have a patched font:
+      -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+      reference_text = { hl_group = 'ObsidianRefText' },
+      highlight_text = { hl_group = 'ObsidianHighlightText' },
+      tags = { hl_group = 'ObsidianTag' },
+      block_ids = { hl_group = 'ObsidianBlockID' },
+      hl_groups = {
+        -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+        ObsidianTodo = { bold = true, fg = '#f78c6c' },
+        ObsidianDone = { bold = true, fg = '#89ddff' },
+        ObsidianRightArrow = { bold = true, fg = '#f78c6c' },
+        ObsidianTilde = { bold = true, fg = '#ff5370' },
+        ObsidianImportant = { bold = true, fg = '#d73128' },
+        ObsidianBullet = { bold = true, fg = '#89ddff' },
+        ObsidianRefText = { underline = true, fg = '#c792ea' },
+        ObsidianExtLinkIcon = { fg = '#c792ea' },
+        ObsidianTag = { italic = true, fg = '#89ddff' },
+        ObsidianBlockID = { italic = true, fg = '#89ddff' },
+        ObsidianHighlightText = { bg = '#75662e' },
+      },
+    },
+    mappings = {
+      ['gd'] = {
+        action = function()
+          return require('obsidian').util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- Toggle check-boxes.
+      ['<leader>ch'] = {
+        action = function()
+          return require('obsidian').util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+    },
+    -- preferred_link_style = 'markdown',
+    ---
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      local suffix = ''
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.time()) .. '-' .. suffix
+    end,
+
+    -- Optional, by default when you use `:ObsidianFollowLink` on a link to an image
+    -- file it will be ignored but you can customize this behavior here.
+    ---@param img string
+    follow_img_func = function(img)
+      vim.fn.jobstart { 'qlmanage', '-p', img } -- Mac OS quick look preview
+    end,
+  },
+}

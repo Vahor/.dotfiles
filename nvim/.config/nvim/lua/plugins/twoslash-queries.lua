a53a1f3a660585b60db68e493f3db49633ae2ff9
Vahor <me@vahor.fr>
2025-10-30 21:50:36 +0100

    add twoslash-queries plugin

---
+++ nvim/.config/nvim/lua/plugins/twoslash-queries.lua
@@ -0,0 +1,16 @@
+return {
+  'marilari88/twoslash-queries.nvim',
+  config = function()
+    require('lspconfig')['tsserver'].setup {
+      on_attach = function(client, bufnr)
+        require('twoslash-queries').attach(client, bufnr)
+      end,
+    }
+    -- Optional: configure the plugin
+    require('twoslash-queries').setup {
+      multi_line = true, -- print types in multi-line mode
+      is_enabled = true, -- enable at startup
+      highlight = 'Type', -- highlight group for virtual text
+    }
+  end,
+}

c67c99315d2b5fc2c26c4e47c36429f3b07324ef
Vahor <me@vahor.fr>
2025-11-03 10:06:16 +0100

    fix lsp setup

--- nvim/.config/nvim/lua/plugins/twoslash-queries.lua
+++ nvim/.config/nvim/lua/plugins/twoslash-queries.lua
@@ -1,11 +1,11 @@
 return {
   'marilari88/twoslash-queries.nvim',
   config = function()
-    require('lspconfig')['tsserver'].setup {
+    vim.lsp.config('ts_ls', {
       on_attach = function(client, bufnr)
         require('twoslash-queries').attach(client, bufnr)
       end,
-    }
+    })
     -- Optional: configure the plugin
     require('twoslash-queries').setup {
       multi_line = true, -- print types in multi-line mode

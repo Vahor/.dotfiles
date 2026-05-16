971e43e99050f6bfe460bf72f5d21647e1844a82
Vahor <me@vahor.fr>
2026-05-16 19:22:19 +0200

    typo

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -5,7 +5,7 @@ return {
     vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Toggle [u]ndo tree' })
 
     vim.g.undotree_WindowLayout = 2
-    vim.g.unditree_ShortIndicators = 1
+    vim.g.undotree_ShortIndicators = 1
     vim.g.undotree_DiffpanelHeight = 15
     vim.g.undotree_SetFocusWhenToggle = 1
   end,

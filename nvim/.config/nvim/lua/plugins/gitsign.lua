be822979ff835774ffb6e461f31e898214d79cff
Vahor <me@vahor.fr>
2026-05-16 18:01:46 +0200

    update git plugins

--- nvim/.config/nvim/lua/plugins/gitsign.lua
+++ nvim/.config/nvim/lua/plugins/gitsign.lua
@@ -12,16 +12,26 @@ return {
           topdelete = { text = '‾' },
           changedelete = { text = '~' },
         },
+        on_attach = function(bufnr)
+          local gitsigns = require 'gitsigns'
+
+          local function map(mode, lhs, rhs, desc)
+            vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+          end
+
+          map('n', '<leader>gp', gitsigns.preview_hunk, 'Preview hunk')
+          map('x', '<leader>gr', ':Gitsigns reset_hunk<CR>', 'Reset visual selection')
+          map('n', '<leader>gb', function()
+            gitsigns.blame_line { full = true }
+          end, 'Blame current line')
+          map('n', '<leader>gB', gitsigns.toggle_current_line_blame, 'Toggle line blame')
+
+          map('n', '<leader>gs', function()
+            vim.cmd 'Gitsigns toggle_deleted'
+            vim.cmd 'Gitsigns toggle_linehl'
+          end, 'Show diff (toggle)')
+        end,
       }
-
-      vim.keymap.set('n', '<leader>gs', function()
-        vim.cmd 'Gitsigns toggle_deleted'
-        vim.cmd 'Gitsigns toggle_linehl'
-      end, { desc = 'Toggle git signs' })
-
-      vim.keymap.set('n', '<leader>gb', function()
-        vim.cmd 'Gitsigns blame_line'
-      end, { desc = 'Blame the current line' })
     end,
   },
 }
--- nvim/.config/nvim/lua/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -8,6 +8,9 @@ return {
       'LazyGitFilter',
       'LazyGitFilterCurrentFile',
     },
+    keys = {
+      { '<leader>gg', '<cmd>LazyGitCurrentFile<cr>', desc = 'Open LazyGit' },
+    },
     -- optional for floating window border decoration
     dependencies = {
       'nvim-lua/plenary.nvim',

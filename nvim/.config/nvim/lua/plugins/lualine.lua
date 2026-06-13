0dab4d7e38f1be0833ba26cda9686540f6cf2c9a
Vahor <me@vahor.fr>
2026-06-13 19:03:46 +0200

    update lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -11,12 +11,46 @@ return {
             return res:sub(1, 3)
           end,
         } },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_b = {
+          {
+            'branch',
+            icon = '',
+            color = { gui = 'bold' },
+          },
+        },
         lualine_c = {
+          {
+            'diff',
+            symbols = { added = '+ ', modified = '= ', removed = '- ' },
+            diff_color = {
+              added = { fg = '#a7c080' },
+              modified = { fg = '#ffdf1b' },
+              removed = { fg = '#ff6666' },
+            },
+          },
+
+          {
+            'diagnostics',
+            sources = { 'nvim_diagnostic' },
+            symbols = {
+              error = ' ',
+              warn = ' ',
+              info = ' ',
+              hint = ' ',
+            },
+            diagnostics_color = {
+              error = { fg = '#ff6666' },
+              warn = { fg = '#ffdf1b' },
+              info = { fg = '#82aaff' },
+              hint = { fg = '#c792ea' },
+            },
+          },
+
           {
             'ex.relative_filename',
             max_length = 0.5,
           },
+          { 'lsp_progress' },
         },
         lualine_x = { 'filetype' },
         lualine_y = {},

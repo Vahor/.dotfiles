af3cfdbc2e564b73466d3630d3743db847cd573a
Nathan <17593209+Vahor@users.noreply.github.com>
2026-06-18 18:59:53 +0200

    Change icon and name for proto file extension

--- nvim/.config/nvim/lua/plugins/icons.lua
+++ nvim/.config/nvim/lua/plugins/icons.lua
@@ -1,14 +1,15 @@
 return {
 	{
 		"nvim-tree/nvim-web-devicons",
+		-- enabled = vim.g.have_nerd_font,
 		config = function()
 			require("nvim-web-devicons").setup({
 				variant = "dark",
 				override_by_extension = {
 					proto = {
-						icon = "",
+						icon = "",
 						color = "#81e043",
-						name = "Log",
+						name = "Proto",
 					},
 				},
 			})

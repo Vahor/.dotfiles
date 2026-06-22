8f2b614db124d55b5c3d9e1d93eed3f6178ce343
Vahor <me@vahor.fr>
2026-06-22 20:05:09 +0200

    add yq dep

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,28 +1,28 @@
 return {
-	{
-		"stevearc/oil.nvim",
-		opts = {
-			watch_for_changes = true,
-			view_options = {
-				show_hidden = true,
-			},
-			keymaps = {
-				["<leader>y"] = {
-					callback = function()
-						local oil = require("oil")
-						local entry = oil.get_cursor_entry()
-						if entry then
-							local dir = oil.get_current_dir()
-							local path = dir .. entry.name
-							vim.fn.setreg("+", path)
-							vim.notify("Copied: " .. path)
-						end
-					end,
-					desc = "Copy file path to system clipboard",
-				},
-			},
-		},
-		-- Optional dependencies
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
+  {
+    'stevearc/oil.nvim',
+    opts = {
+      watch_for_changes = true,
+      view_options = {
+        show_hidden = true,
+      },
+      keymaps = {
+        ['<leader>y'] = {
+          callback = function()
+            local oil = require 'oil'
+            local entry = oil.get_cursor_entry()
+            if entry then
+              local dir = oil.get_current_dir()
+              local path = dir .. entry.name
+              vim.fn.setreg('+', path)
+              vim.notify('Copied: ' .. path)
+            end
+          end,
+          desc = 'Copy file path to system clipboard',
+        },
+      },
+    },
+    -- Optional dependencies
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
 }

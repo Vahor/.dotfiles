d2e02916a9551a497a9dc11579bab5013030f8b3
Nathan <17593209+Vahor@users.noreply.github.com>
2026-06-22 10:47:06 +0200

    Add keymap for search resume functionality

--- nvim/.config/nvim/lua/plugins/fff.lua
+++ nvim/.config/nvim/lua/plugins/fff.lua
@@ -23,7 +23,8 @@ return {
         status_text_color = true,
       },
     }
-
+    
+		vim.keymap.set("n", "<leader>sr", fff.resume, { desc = "[S]earch [R]esume" })
     vim.keymap.set('n', '<leader>sf', fff.find_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>sg', fff.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sn', function()
